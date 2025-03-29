90e33b18907c1f293283d941d03d32da8dfbcd2e
mauv15 <mauriciovc1545@gmail.com>
2025-03-29 13:52:01 -0600

    youtube videos

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Introduction to C#", "Dani Krossing", 340);
+        video1.AddComment(new Comment("Alice", "Great explanation!"));
+        video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Charlie", "I love this content!"));
+
+        Video video2 = new Video("Every single feature of C# in 10 minutes", "Train To Code", 590);
+        video2.AddComment(new Comment("David", "This was really insightful."));
+        video2.AddComment(new Comment("Eve", "I never knew about these features!"));
+        video2.AddComment(new Comment("Frank", "Clear and concise, thank you."));
+
+        Video video3 = new Video("5 Design Patterns That Are ACTUALLY Used By Developers", "Alex Hyett", 566);
+        video3.AddComment(new Comment("Grace", "Super useful for my project!"));
+        video3.AddComment(new Comment("Hank", "I need to rewatch this!"));
+        video3.AddComment(new Comment("Ivy", "Best explanation I've seen."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    public List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.Name}: {comment.Text}");
+        }
+        Console.WriteLine();
+    }
+}
